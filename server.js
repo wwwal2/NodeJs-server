d2dbbda564e8632606aeab58bada61953a7398c2
wwwal2 <smalex9@gmail.com>
2018-07-17 12:02:55 +0300

    setting the heroku deployment

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const helpFromNpm = require('express');
 const helpFromNpm2 = require('hbs');
+const helpFromCore = require('fs');
+
+const herokuPort = process.env.PORT || 3000;                        //making dynamic port for heroku
 
 let app = helpFromNpm();
+
+app.use((req, res, next) => {                                       //making operations with express requests
+    let showDate = new Date().toString();
+    let log = `${showDate}: ${req.method} ${req.url} `;
+    console.log(log);
+    helpFromCore.appendFileSync('logsOfTheServer.log', log + '\n');
+    next();
+});
 
 
 //using HBS
 app.set('view engine', 'helpFromNpm2');
-helpFromNpm2.registerPartials(__dirname + '/views/partials');        //setting partials dir
 
+helpFromNpm2.registerPartials(__dirname + '/views/partials');        //setting partials dir
 helpFromNpm2.registerHelper('getCurrentDate', () => {               //injecting function in hbs
     return new Date()
 });
-
 helpFromNpm2.registerHelper('screamFunc', (text) => {
    return text.toUpperCase();
 });
@@ -33,7 +43,7 @@ app.get('/directServerPage', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('The server is running at localhost:3000/staticTitlePage.html');
+app.listen(herokuPort, () => {
+    console.log(`The server is running at localhost:${herokuPort}/titleHBS`);
     console.log('List of pages available: staticTitlePage.html, directServerPage, titleHBS');
 });
